d316e46ffc4ad669a40a8dbdf9748963a9d7d0a6
ImanBurluk <jaroslavzev63@mail.ru>
2025-07-13 22:02:38 +0500

    Make TODO in ProfileDao and ProfileService

--- src/main/java/ru/iman_burlyq/dao/ProfileDao.java
+++ src/main/java/ru/iman_burlyq/dao/ProfileDao.java
@@ -2,13 +2,18 @@ package ru.iman_burlyq.dao;
 
 import ru.iman_burlyq.model.Profile;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class ProfileDao {
+
     private final ConcurrentHashMap<Long, Profile> storage;
+
     private final AtomicLong idStorage;
+
     public ProfileDao() {
         this.storage = new ConcurrentHashMap<>();
         this.idStorage = new AtomicLong();
@@ -26,4 +31,22 @@ public class ProfileDao {
     }
 
     // TODO delete, update, findAll
+    public Optional<Profile> update(Profile profile) {
+        Long id = profile.getId();
+        if (id == null || !storage.containsKey(id)) {
+            return Optional.empty();
+        }
+        storage.put(id, profile); // заменяем старый профиль
+        return Optional.of(profile);
+    }
+
+    public boolean delete(Long id) {
+        return storage.remove(id) != null;
+    }
+
+    public List<Profile> findAll() {
+        return new ArrayList<>(storage.values());
+    }
+
+
 }
--- src/main/java/ru/iman_burlyq/service/ProfileService.java
+++ src/main/java/ru/iman_burlyq/service/ProfileService.java
@@ -3,6 +3,7 @@ package ru.iman_burlyq.service;
 import ru.iman_burlyq.dao.ProfileDao;
 import ru.iman_burlyq.model.Profile;
 
+import java.util.List;
 import java.util.Optional;
 
 public class ProfileService {
@@ -23,4 +24,22 @@ public class ProfileService {
     }
 
     // TODO delete, update, findAll
+    public Optional<Profile> update(Profile profile) {
+        if (profile == null || profile.getId() == null) return Optional.empty();
+        return dao.update(profile);
+    }
+
+    public boolean delete(Long id) {
+        if (id == null) {
+            return false;
+        }
+
+        return dao.delete(id);
+    }
+
+
+    public List<Profile> findAll() {
+        return dao.findAll();
+    }
+
 }
