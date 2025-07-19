2c7efb457620c7385d14a9add16207bb4d4f1d9b
ImanBurluk <jaroslavzev63@mail.ru>
2025-07-19 22:55:31 +0500

    HTTP Server

--- src/main/java/ru/iman_burlyq/CharmBackServerRunner.java
+++ src/main/java/ru/iman_burlyq/CharmBackServerRunner.java
@@ -4,48 +4,13 @@ import ru.iman_burlyq.controller.ProfileController;
 import ru.iman_burlyq.dao.ProfileDao;
 import ru.iman_burlyq.service.ProfileService;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
-
-import static java.io.DataInputStream.readUTF;
-import static ru.iman_burlyq.model.Commands.*;
 
 public class CharmBackServerRunner {
     public static void main(String[] args) throws IOException {
 
         ProfileController controller = new ProfileController(new ProfileService(new ProfileDao()));
-
-        try (ServerSocket serverSocket = new ServerSocket(8080);
-             Socket socket = serverSocket.accept();
-             DataInputStream requestStream = new DataInputStream(socket.getInputStream());
-             DataOutputStream responseStream = new DataOutputStream(socket.getOutputStream());
-        ) {
-            String request = requestStream.readUTF();
-            String response;
-
-            while(!"stop".equals(request)) {
-                if(request.startsWith(SAVE.getPrefix())) {
-                    response = controller.save(request.split(SAVE.getPrefix())[1]);
-                } else if (request.startsWith(FIND_BY_ID.getPrefix())) {
-                    response = controller.findById(request.split(FIND_BY_ID.getPrefix())[1]);
-                } else if (request.startsWith(FIND_ALL.getPrefix())) {
-                    response = controller.findAll();
-                } else if (request.startsWith(UPDATE.getPrefix())) {
-                    response = controller.update(request.split(UPDATE.getPrefix())[1]);
-                } else if (request.startsWith(DELETE.getPrefix())) {
-                    response = controller.delete(request.split(DELETE.getPrefix())[1]);
-                }  else {
-                    response = "Unsupported operation";
-                }
-                // TODO update, delete, findById, findAll
-
-                responseStream.writeUTF(response);
-                request = requestStream.readUTF();
-            }
-        }
+        CharmHttpServer server = new CharmHttpServer(5);
+        server.start();
     }
 }
---
+++ src/main/java/ru/iman_burlyq/CharmHttpServer.java
@@ -0,0 +1,56 @@
+package ru.iman_burlyq;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CharmHttpServer {
+    private ExecutorService executorService;
+
+    public CharmHttpServer(int poolSize) {
+        this.executorService = Executors.newFixedThreadPool(poolSize);
+    }
+
+    public void start() throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+            while (true) {
+                Socket connection = serverSocket.accept();
+                System.out.println("-----------Client connect------------");
+                executorService.submit(() -> {
+                    processConnection(connection);
+                });
+            }
+
+
+        }
+    }
+
+    private void processConnection(Socket connection){
+        try (connection;
+             BufferedReader rqReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+             DataOutputStream rsStream = new DataOutputStream(connection.getOutputStream());
+        ) {
+           while (!rqReader.ready()) {
+               while (rqReader.ready()) {
+                   System.out.println(rqReader.readLine());
+               }
+               byte[] body = "Hi from CharmServer".getBytes();
+               byte[] startSting = "HTTP/1.1 200 OK\n".getBytes();
+               byte[] headers = "Content-Type: text/plain\nContent-Length: %s\n\n".formatted(body.length).getBytes();
+
+               rsStream.write(startSting);
+               rsStream.write(headers);
+               rsStream.write(body);
+
+               System.out.println("-----------Client disconnect------------");
+           }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
